22cce142e210f8656399b70993eeb08959115a8f
Jacopo-Surrey <j.magini@surrey.ac.uk>
2021-10-20 11:20:52 +0100

    Readability fixes to data analysis

--- plot_fd.py
+++ plot_fd.py
@@ -1,27 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import glob
+
+def Normalize(f, x) :
+	dx = x[1:] - x[:-1]
+	integral = np.sum(f[:-1] * dx)
+	f_norm = f[:-1] / integral
+	return f_norm
 
 # in cvs each thread outputs to a different file
 # therefore I need to cycle through each thread and append them all together
 
-ntuple_name = "output_nt_2_deposited_energy"
-
-n_threads = 4
-
 energy = np.empty([0,1])
 length = np.empty([0,1])
 
-for i in range(0, n_threads) :
-	
-	file_name = ntuple_name + "_t" + str(i) + ".csv"
-	
-	energy_thread, length_thread = np.loadtxt(file_name, delimiter=',', unpack=True)
+ntuple_name = "output_nt_2_deposited_energy"
+output_name = ntuple_name + "_t" + "*" + ".csv"
+output_list = glob.glob(output_name)
+
+for this_file in output_list :	
+	energy_thread, length_thread = np.loadtxt(this_file, delimiter=',', unpack=True)
 	
 	energy = np.append(energy, energy_thread)
 	length = np.append(length, length_thread)
 
 mean_path_length = np.average(length)
-	
+
 y = energy / mean_path_length
 
 
@@ -35,7 +39,7 @@ exp_end = np.ceil(np.log10(maximum))
 
 n_decades = int(exp_end - exp_start)
 
-bins_per_dec = 300	# change this depending on how many points you have
+bins_per_dec = 60	# change this depending on how many points you have
 
 n_bins = n_decades * bins_per_dec
 
@@ -49,32 +53,19 @@ for i in range(1, n_bins) :
 
 # create the histogram
 
-f = np.histogram( y, bins=y_bins ) [0]	# now f is a number of counts...
+f = np.histogram( y, bins=y_bins ) [0]	# for now f is a number of counts...
 
-tot_counts = str(np.sum(f))
+tot_counts = np.sum(f)
 
-for i in range(n_bins-1) :	# f has dimension n_bins-1
-		
-		# ... so I turn it into a density
-		bin_width = y_bins[i+1] - y_bins[i]	
-		f[i] = f[i] / bin_width
-		
-		# I also shift the abscissa to the middle of the bin
-		y_bins[i] = y_bins[i] * 10**( 1 / bins_per_dec / 2 )
+# ... so now I turn f into a density
+bin_width = y_bins[1:] - y_bins[:-1]
+f = f / bin_width
 
-y_bins = y_bins[0:-1]	#remove the last point,
-							# so both arrays have the same size
-
+f = np.append(f, 0.)	# give f and y_bins arrays the same size
 
 # normalize the spectra
-
-def normalize(f, y) :
-	integral = np.trapz(f, y)
-	f_norm = f / integral
-	return f_norm
-
-f = normalize(f, y_bins)
-d = normalize(y_bins*f, y_bins)
+f = Normalize(f, y_bins)
+d = Normalize(y_bins*f, y_bins)
 
 
 # save to file
@@ -106,7 +97,7 @@ ax2.semilogx( y_bins, y_bins * d, linewidth=0.5, color=color )
 ax2.set_ylabel(r'$y \cdot d(y) $', color=color)
 ax2.tick_params(axis='y', labelcolor=color)
 
-title = tot_counts + " counts, " + str(bins_per_dec) + " bins per decade"
+title = str(tot_counts) + " counts, " + str(bins_per_dec) + " bins per decade"
 fig.suptitle(title)
 
 fig.tight_layout()
